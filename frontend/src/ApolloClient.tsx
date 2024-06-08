ae75419ec600cb4bc2eb00b4d696270931430c68
Kamaliki Clive <47914903+kamaliki@users.noreply.github.com>
2024-06-08 17:20:25 +0300

    Create ApolloClient.tsx
    Connect to the data

---
+++ frontend/src/ApolloClient.tsx
@@ -0,0 +1,20 @@
+/**
+ * Set up Apollo Client
+ * Link to the GraphQL API
+ */
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+/**
+ * Create a new Apollo Client
+ * The function takes an object with two properties:
+ * uri: The URL of the GraphQL server
+ * cache: The cache that Apollo Client will use to store the data
+ * In this case, we're using the InMemoryCache, which is a normalized cache that stores data in memory
+ * The cache is used to store the results of queries and mutations, so that they can be reused later
+ */
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
+
+export default client;
